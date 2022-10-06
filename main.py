8aa0f377820d27a3f42079e73e87b210cedbe3c2
GoldenbyteGH <obertigiovannicesare@gmai.com>
2022-10-06 18:29:15 +0200

    configuration

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
-import socket
+import socket,configparser
+from requests import patch
 from netbox import NetBox
 
-
 class Device:
-    def __init__(self,name,domain='.goldenbyte.it',SM='/24'):
+    def __init__(self,name,domain,SM='/24'):
         self.name=name+domain
         self.IP='0.0.0.0'
         self.subnet=SM
@@ -20,24 +20,40 @@ class Device:
 
 
 if __name__ == '__main__':
+    ####################################################
+    # config Netbox AuthInfo
+    ####################################################
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    netbox_srv=config['NETBOX_SRV']['url']
+    netbox_srv_header={
+        'Accept':'application/json',
+        'Authorization':config['NETBOX_SRV']['token'],
+    }
+    ##################################################
 
-    netbox_device = NetBox(host='netbox.goldenbyte.it', use_ssl=False, auth_token='2987124c7bfb33f28ddc7872599acddf13bf3d6c')
+    netbox_device = NetBox(host=netbox_srv[8:], use_ssl=False, auth_token=netbox_srv_header['Authorization'][6:])
     name_fdevices = netbox_device.dcim.get_devices()
     name_vdevices = netbox_device.virtualization.get_virtual_machines()
 
     for item in netbox_device.ipam.get_ip_addresses():
         if item['assigned_object_id']!= None and item['status']['value'] == 'dhcp' :
             try:
-                v_device = Device(item['assigned_object']['virtual_machine']['name'])
+                v_device = Device(item['assigned_object']['virtual_machine']['name'],domain=config['NETBOX_SRV']['domain'])
                 print('virtual',item['address'],v_device.name,'<=====>',v_device.getIP())
-                if item['address'][:-3] != v_device.getIP():
-                    print(v_device.getIP()+v_device.getSM())
-                    exit()
-                    netbox_device.ipam.update_ip(item['address'],{'address':v_device.getIP()+v_device.getSM()})
+                if item['address'][:-3] != v_device.getIP() and v_device.getIP() != None:
+                    payload = {
+                        'address': v_device.getIP()+v_device.getSM()
+                    }
+                    patch(url=netbox_srv+'/api/ipam/ip-addresses/'+str(item['id'])+'/',headers=netbox_srv_header,json=payload,verify=False)
             except:
                 f_device = Device(item['assigned_object']['device']['name'])
                 print('physical',item['address'],f_device.name,'<=====>',f_device.getIP())
-                if item['address'][:-3] != f_device.getIP():
-                    netbox_device.ipam.update_ip(item['address'],{'address':f_device.getIP()+f_device.getSM()})
+                if item['address'][:-3] != f_device.getIP() and f_device.getIP() != None:
+                    payload = {
+                        'address': f_device.getIP()+f_device.getSM()
+                    }
+                    patch(url=netbox_srv+'/api/ipam/ip-addresses/'+str(item['id'])+'/',headers=netbox_srv_header,json=payload,verify=False)
+
 
 
